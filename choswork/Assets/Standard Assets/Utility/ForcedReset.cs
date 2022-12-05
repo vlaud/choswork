453e67dd47f8356511f43f2b3fcfedb616cbb207
vlaud <whehdgus1207@naver.com>
2022-12-06 07:47:09 +0900

    download

---
+++ choswork/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof (Image))]
+public class ForcedReset : MonoBehaviour
+{
+    private void Update()
+    {
+        // if we have forced a reset ...
+        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+        {
+            //... reload the scene
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+        }
+    }
+}
