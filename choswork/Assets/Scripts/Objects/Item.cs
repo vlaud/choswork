5d199e50e3587bb11da11e29dce321d091b2678a
vlaud <whehdgus1207@naver.com>
2023-01-11 21:55:12 +0900

    인벤토리 작업중

--- choswork/Assets/Scripts/Objects/Item.cs
+++ choswork/Assets/Scripts/Objects/Item.cs
@@ -16,6 +16,7 @@ public class Item : ScriptableObject
     public string itemName; // 아이템의 이름
     public ItemType itemType; // 아이템 유형
     public Sprite itemImage; // 아이템의 이미지(인벤 토리 안에서 띄울)
+    public Texture itemTex;
     public GameObject itemPrefab;  // 아이템의 프리팹 (아이템 생성시 프리팹으로 찍어냄)
 
     public string weaponType;  // 무기 유형
