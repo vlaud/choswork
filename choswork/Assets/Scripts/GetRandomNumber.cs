8f65b0c96d6d122c3f971ece7cab8b1b895d67af
vlaud <whehdgus1207@naver.com>
2023-02-03 21:16:57 +0900

    난수생성 클래스 추가

---
+++ choswork/Assets/Scripts/GetRandomNumber.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GetRandomNumber
+{
+    public static List<int> GetRanNum(int StartNum, int EndNum, int ResultAmounts, 
+        bool RemoveStartnNum, int[] RemoveNums = null)
+    {
+        // 중복 숫자 방지
+        List<int> Numbers = new List<int>();
+        for (int i = StartNum; i < EndNum; ++i)
+        {
+            Numbers.Add(i);
+        }
+        if(RemoveStartnNum && RemoveNums != null)
+        {
+            for(int i = 0; i < RemoveNums.Length; ++i)
+            {
+                Numbers.Remove(RemoveNums[i]);
+                Debug.Log("Removed: " + RemoveNums[i]);
+            }
+        }
+        List<int> LottoNumber = new List<int>();
+        for (int i = 0; i < ResultAmounts; ++i)
+        {
+            int n = UnityEngine.Random.Range(0, Numbers.Count);
+            LottoNumber.Add(Numbers[n]);
+            Numbers.RemoveAt(n);
+        }
+        return LottoNumber;
+    }
+}
