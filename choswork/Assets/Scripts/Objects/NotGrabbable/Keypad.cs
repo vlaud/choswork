858c1bed9659b4f6fad16d7def60729766188e61
vlaud <whehdgus1207@naver.com>
2023-02-04 21:15:21 +0900

    키패드 번호 입력

--- choswork/Assets/Scripts/Objects/NotGrabbable/Keypad.cs
+++ choswork/Assets/Scripts/Objects/NotGrabbable/Keypad.cs
@@ -2,33 +2,63 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public struct KeypadStat
 {
     public GameObject keypadUI;
     public GameObject hintNote;
-    public TMPro.TMP_Text passwordInput;
 }
 public class Keypad : ObjectNotGrabbable
 {
-    [SerializeField] protected TMPro.TMP_Text passwordText;
+    [SerializeField] protected TMPro.TMP_Text hintText;
+    [SerializeField] protected TMPro.TMP_Text passwordInput;
     public KeypadStat myKeypad;
     public Transform myHintNote;
     public Transform myKeypadUI;
+    public Button[] myButtons;
+    private string Nr = null;
+    private int NrIndex = 0;
     private void Awake()
     {
         SetActionText();
         myKeypadUI = Instantiate(myKeypad.keypadUI, GameManagement.Inst.myCanvas.transform).transform;
+        if (passwordInput == null) passwordInput = myKeypadUI.Find("KeyInput").GetComponentInChildren<TMPro.TMP_Text>();
         myHintNote = Instantiate(myKeypad.hintNote).transform;
         var canvas = myHintNote.GetComponentInChildren<Canvas>();
-        if (passwordText == null) passwordText = canvas.transform.Find("passwordtext").GetComponent<TMPro.TMP_Text>();
+        if (hintText == null) hintText = canvas.transform.Find("passwordtext").GetComponent<TMPro.TMP_Text>();
         List<int> passNums = GetRandomNumber.GetRanNum(1, 10, 4, false);
         string Password = "";
         foreach (int num in passNums)
         {
             Password += num.ToString();
         }
-        passwordText.text = Password;
+        myButtons = myKeypadUI.GetComponentsInChildren<Button>();
+        ButtonClick();
+        hintText.text = Password;
+        //DisableUI();
+    }
+    public override void Interact()
+    {
+        myKeypadUI.gameObject.SetActive(true);
+    }
+    public void DisableUI()
+    {
+        myKeypadUI.gameObject.SetActive(false);
+    }
+    public void CodeFunction(string Number)
+    {
+        NrIndex++;
+        Nr = Nr + Number;
+        passwordInput.text = Nr;
+    }
+    void ButtonClick()
+    {
+        for(int i = 0; i < 10; ++i)
+        {
+            int x = i; //원인 불명으로 i를 바로 쓰면 죄다 10으로 변함
+            myButtons[x].onClick.AddListener(delegate { CodeFunction(x.ToString()); });
+        }
     }
 }
