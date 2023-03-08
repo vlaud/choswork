52cb3962dc78a30e4ae1a8c9d2c1941a28238dfe
vlaud <whehdgus1207@naver.com>
2023-03-08 13:07:23 +0900

    행동트리 ui 추가

---
+++ choswork/Assets/Editor/InspectorView.cs
@@ -0,0 +1,10 @@
+using UnityEngine.UIElements;
+
+public class InspectorView : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<InspectorView, VisualElement.UxmlTraits> { }
+    public InspectorView()
+    {
+
+    }
+}
