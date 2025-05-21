7d3cd1bfd56ce1b1c9f045db1d3d7d99423ef66b
vlaud <whehdgus1207@naver.com>
2025-05-21 13:53:36 +0900

    난수 생성 함수 수정
    Fisher-Yates 셔플을 통해 숫자를 무작위로 섞은 뒤 숫자 개수만큼 반환
    
    기존의 방법 : 숫자 리스트를 만든 뒤에 랜덤한 위치의 숫자를 뽑고 그 부분을 지우는 방식
    
    문제 : 매번 숫자를 뽑을때마다 RemoveAt을 사용하면 배열의 이동이 일어나 리스트가 클수록 느려짐
    반환해야 할 결과 개수 > 유효한 숫자의 예외를 놓침
    
    해결 : Fisher-Yates 알고리즘 활용
    리스트 순서만 섞고 앞에서 N개를 추출
    
    Fisher-Yates 작동 원리 :
    1. 마지막 요소부터 시작해 각 요소를 랜덤으로 선택된 앞쪽 요소와 위치 교환
    2. 첫번째 요소까지 과정 반복 (i > 0)

--- Assets/Scripts/Base/GetRandomNumber.cs
+++ Assets/Scripts/Base/GetRandomNumber.cs
@@ -1,88 +1,60 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class GetRandomNumber
 {
     /// <summary>
-    /// 랜덤 숫자 목록 생성
+    /// 난수 리스트 생성
     /// </summary>
-    /// <param name="StartNum">시작 번호</param>
-    /// <param name="EndNum">끝 번호 (배열, 리스트 사이즈)</param>
-    /// <param name="ResultAmounts">받고 싶은 숫자의 개수</param>
-    /// <param name="RemoveNums">제외할 숫자들</param>
+    /// <param name="StartNum">시작</param>
+    /// <param name="EndNum">끝</param>
+    /// <param name="ResultAmounts">숫자 개수</param>
+    /// <param name="RemoveNums">지울 숫자들</param>
+    /// <param name="seed">시드</param>
     /// <returns></returns>
-    public static List<int> GetRanNums(int StartNum, int EndNum, int ResultAmounts,
-        int[] RemoveNums = null)
+    /// <exception cref="ArgumentException"></exception>
+    public static List<int> GetRanNums(int StartNum, int EndNum, int ResultAmounts, int[] RemoveNums = null, int? seed = null)
     {
-        // 중복 숫자 방지
-        List<int> Numbers = new List<int>();
-        for (int i = StartNum; i < EndNum; ++i)
-        {
-            Numbers.Add(i);
-        }
+        // 시작 숫자가 끝 숫자보다 크거나 같으면 throw
+        if (StartNum >= EndNum)
+            throw new ArgumentException("StartNum은 EndNum보다 작아야 함.");
 
+        // StartNum ~ EndNum-1까지 리스트 생성
+        var range = Enumerable.Range(StartNum, EndNum - StartNum).ToList();
+
+        // 지울 숫자들이 있으면 지움
         if (RemoveNums != null)
+            foreach (var num in RemoveNums)
+                range.Remove(num);
+
+        // 반환해야 할 숫자 개수가 리스트보다 크면 throw
+        if (ResultAmounts > range.Count)
+            throw new ArgumentException($"ResultAmounts({ResultAmounts}) > available range size({range.Count}).");
+
+        // 시드값이 존재하면 시드 위주로, 아니면 시스템 값으로
+        var rng = seed.HasValue ? new Random(seed.Value) : new Random();
+
+        // Fisher-Yates 셔플
+        for (int i = range.Count - 1; i > 0; i--)
         {
-            for (int i = 0; i < RemoveNums.Length; ++i)
-            {
-                Numbers.Remove(RemoveNums[i]);
-            }
+            int j = rng.Next(i + 1);
+            (range[i], range[j]) = (range[j], range[i]);
         }
 
-        List<int> LottoNumber = new List<int>();
-        for (int i = 0; i < ResultAmounts; ++i)
-        {
-            int n = UnityEngine.Random.Range(0, Numbers.Count);
-            LottoNumber.Add(Numbers[n]);
-            Numbers.RemoveAt(n);
-        }
-        return LottoNumber;
+        return range.GetRange(0, ResultAmounts);
     }
 
     /// <summary>
-    /// 랜덤 숫자 1개 생성
+    /// 단일 난수 생성
     /// </summary>
-    /// <param name="StartNum">시작 번호</param>
-    /// <param name="EndNum">끝 번호</param>
-    /// <param name="RemoveNums">제외할 숫자들</param>
+    /// <param name="StartNum"></param>
+    /// <param name="EndNum"></param>
+    /// <param name="RemoveNums"></param>
+    /// <param name="seed"></param>
     /// <returns></returns>
-    public static int GetRanNum(int StartNum, int EndNum, int[] RemoveNums = null)
+    public static int GetRanNum(int StartNum, int EndNum, int[] RemoveNums = null, int? seed = null)
     {
-        // 중복 숫자 방지
-        List<int> Numbers = new List<int>();
-        for (int i = StartNum; i < EndNum; ++i)
-        {
-            Numbers.Add(i);
-        }
-
-        if (RemoveNums != null)
-        {
-            for (int i = 0; i < RemoveNums.Length; ++i)
-            {
-                Numbers.Remove(RemoveNums[i]);
-            }
-        }
-
-        return UnityEngine.Random.Range(0, Numbers.Count);
-    }
-
-    /// <summary>
-    /// 랜덤 숫자 1개 생성
-    /// </summary>
-    /// <param name="StartNum">시작 번호</param>
-    /// <param name="EndNum">시작 번호</param>
-    /// <param name="RemoveNum">제외할 숫자</param>
-    /// <returns></returns>
-    public static int GetRanNum(int StartNum, int EndNum, int RemoveNum)
-    {
-        // 중복 숫자 방지
-        List<int> Numbers = new List<int>();
-        for (int i = StartNum; i < EndNum; ++i)
-        {
-            Numbers.Add(i);
-        }
-
-        Numbers.Remove(RemoveNum);
-       
-        return UnityEngine.Random.Range(0, Numbers.Count);
+        return GetRanNums(StartNum, EndNum, 1, RemoveNums, seed)[0];
     }
 }
